2a59fb1964392daa07050a4d4bdb3198ded71b9b
Felipe-dSantos <lipehh18@gmail.com>
2023-12-08 18:40:27 -0300

    update settings

--- SDRA/settings.py
+++ SDRA/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'SDRA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,16 +144,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
+
+# STATIC_ROOT = os.path.join(os.path.dirname(
+#     BASE_DIR), "staticfiles")
+
+
+
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'static')
 ]
-
-STATIC_ROOT = os.path.join(os.path.dirname(
-    BASE_DIR), "static", "staticfiles")
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'staticfiles') 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
