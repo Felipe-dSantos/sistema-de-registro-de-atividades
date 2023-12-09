1362af154d317ed9ddc91c8b2f46976437ab29ed
Felipe-dSantos <lipehh18@gmail.com>
2023-12-09 01:55:36 -0300

    Update settings media

--- core/views.py
+++ core/views.py
@@ -547,8 +547,7 @@ def export_pdf(request):
 
    # Adiciona o cabeçalho com a imagem e o título
     # header_frame = Frame(inch, doc.height + inch, doc.width, inch)
-    image_path = os.path.join(
-        settings.BASE_DIR, 'staticfiles', 'img', 'Logo-ufac-cor.1f271a07facd.png')
+    image_path = os.path.join(settings.MEDIA_ROOT, 'img/Logo-ufac-cor.png')
     logo = Image(image_path, width=40, height=56)
 
     # Título
@@ -825,8 +824,7 @@ def gerar_pdf_relatorio(request, pk):
     )
 
     # Adiciona o cabeçalho com a imagem e o título
-    image_path = os.path.join(
-        settings.BASE_DIR, 'static', 'img', 'Logo-ufac-cor.1f271a07facd.png')
+    image_path = os.path.join(settings.MEDIA_ROOT, 'img/Logo-ufac-cor.png')
     logo = Image(image_path, width=40, height=56),
     titulo = Paragraph(
         '<b>REGISTRO DE ATIVIDADES REALIZADAS<br/>NO LABORATÓRIO DE INFORMÁTICA DO LIFE</b>', style_title)
