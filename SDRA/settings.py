707cfd53bdc8028aa45409bcc3c7e517b0fbf4da
Felipe Dos Santos <99162735+Felipe-dSantos@users.noreply.github.com>
2024-06-07 10:38:27 -0300

    Update settings.py

--- SDRA/settings.py
+++ SDRA/settings.py
@@ -168,8 +168,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lds.dossantos1@gmail.com'
-EMAIL_HOST_PASSWORD = 'panzapmggxiqhwty'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 MESSAGE_STORAGE_OPTIONS = {
